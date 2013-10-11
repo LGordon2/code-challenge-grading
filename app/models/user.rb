30344f6495cfdec6c3698a7646a5c36e8ba5b39d
lew <LGordon2@gmail.com>
2013-10-11 09:51:23 -0400

    Challenge pages now have the correct template.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,6 @@ class User < ActiveRecord::Base
 
   def self.authenticate(username, password)
     user = User.find_by_username(username)
-    if user && user.password == password
-      user
-    else
-      nil
-    end
+    return user && user.password == password ? user : nil
   end
 end
