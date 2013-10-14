cfc044ba7b04e50b64b51b23bd7353158cdf3cce
Matt Watson <Watson.MattC@Gmail.com>
2013-10-13 22:10:07 -0400

    Fixed issue with submit button on file submission panel.  Also added niffty loading message during vbscript eval.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,6 +25,17 @@ function getFilePath() {
 }
 
 function clickSubmitButton() {
-	document.getElementById("submissionForm").submit();
+    $('#submitButtonPanel').fadeOut("slow",function(){
+        $('#loadingPanel').fadeIn("slow").delay(2000,function(){
+
+            document.getElementById("submissionForm").submit();
+
+
+        });
+    });
+
+
+
+
 }
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
