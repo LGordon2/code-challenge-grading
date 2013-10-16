a8ae8dce17fba96a6bf3d0ee759add2f84913306
lew <LGordon2@gmail.com>
2013-10-16 16:15:00 -0400

    Code cleanup, a back button, and some overall fixes.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,7 +4,7 @@ class AdminController < ApplicationController
     user.password = params[:user][:password]
     user.password_confirmation = params[:user][:password_confirmation]
     if(user.save)
-      render action: 'index'
+      redirect_to :admin
     else
       flash[:errors] = user.errors
       redirect_to :back 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ActiveRecord::Base
   has_many :submissions, dependent: :destroy
   has_many :comments, dependent: :destroy
   validates :username, presence:true, uniqueness: {case_sensitive: false}
-
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
+  validates :password, presence:true, confirmation: true
+  
   VALID_EMAIL_REGEX = %r{[a-zA-Z]+\.[a-zA-Z]+\@orasi\.com}
   validates_format_of :username, :with => VALID_EMAIL_REGEX , :message => "must be Orasi email address."
 
