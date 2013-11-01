9313531b7f347034918d49b2dd426efcec2af206
unknown <Orasi@GSOL-Loaner1.orasi.com>
2013-10-31 22:28:58 -0500

    Fixed and updated test cases. Added support for login without @orasi.com, can now use full email or just name Added basic test case error handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,18 @@ class User < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :username, presence:true, uniqueness: {case_sensitive: false}
   
-  VALID_EMAIL_REGEX = %r{[a-zA-Z]+\.[a-zA-Z]+\@orasi\.com}
-  validates_format_of :username, :with => VALID_EMAIL_REGEX , :message => "must be Orasi email address."
+  VALID_EMAIL_REGEX = %r{[a-zA-Z]+\.[a-zA-Z]+\@orasi\.com|[a-zA-Z]+\.[a-zA-Z]+}
+  validates_format_of :username, :with => VALID_EMAIL_REGEX , :message => "must be Or2asi email address or Orasi Active Directory credentials."
 
   #TODO this needs clean up bad...
   def self.authenticate(username, password)
-     first_part_username,_ = username.split('@')  
+    
+     if username.downcase.include?  '@orasi.com'
+	first_part_username,_ = username.split('@') 
+     else     
+	 first_part_username = username
+     end
+
      ldap = Net::LDAP.new :host => '10.238.242.32',
      :port => 389,
      :auth => {
@@ -19,13 +25,13 @@ class User < ActiveRecord::Base
            :password => password
      }
      if ldap.bind 
-       user = User.find_by_username(username.downcase)
+       user = User.find_by_username(first_part_username.downcase)
        if user 
          return user 
        else 
          user = User.new
-         user.username = username.downcase
-         user.first_name,user.last_name = user.username.split('@').first.split('.')
+         user.username = first_part_username.downcase
+         user.first_name,user.last_name = first_part_username.downcase.split('.')
          user.save
          return user
        end
