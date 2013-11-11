bf0b187674dac06a8ab270fd737b9effe95ec5b6
gso-admin <gso-admin@gso-ubuntu.(none)>
2013-11-11 14:34:25 -0500

    Current production snapshot

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,16 +7,6 @@ class UserTest < ActiveSupport::TestCase
     assert user.save
   end
   
-  test "should be valid orasi email" do
-    user = User.new
-    user.username = "test2@orasi.com"
-    assert !user.save
-    user.username = "test2@gmail.com"
-    assert !user.save
-    user.username = "test.lew@gmail.com"
-    assert !user.save
-  end
-  
   test "should not be able to create user without username" do
     user = User.new
     assert !user.save
