35c9612169cbd21178d94b618300b466d57b441c
gso-admin <gso-admin@gso-ubuntu.(none)>
2013-11-15 09:02:13 -0500

    Most recent production snapshot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
-
+  protect_from_forgery with: :exception
+  force_ssl unless Rails.env.development?
   helper_method :current_user
   helper_method :current_comments
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,11 @@
 class WelcomeController < ApplicationController
   before_action :require_login, except: :login
-  force_ssl only: :login, if: :ssl_configured?
   
   def current_user_photo_file
     f = Tempfile.new(['test','.jpg'], Rails.root.join('app','assets','images'),:encoding => 'utf-8')
     f.puts current_user.photo
     f.close
     return f.path
-  end
-  
-  private
-  
-  def ssl_configured?
-    !Rails.env.development?
   end
   
 end
