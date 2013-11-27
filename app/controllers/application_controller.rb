25e222ea9003222c6a5791a959464cb0675eb776
lew <LGordon2@gmail.com>
2013-11-27 13:37:21 -0500

    Hotfix to login error issue.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,6 @@ class ApplicationController < ActionController::Base
   
   def require_login
     unless current_user
-      flash[:error] = "You must be logged in to access this section"
       redirect_to :login # halts request cycle
     end
   end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -8,13 +8,17 @@ class WelcomeControllerTest < ActionController::TestCase
   
   test "should not see index" do
     get :index
-    assert_response :redirect
-    assert_not_nil flash[:error]
+    assert_redirected_to :login
   end
   
   test "should be logged in and see index" do
     get :index, nil, {'user_id' => users(:lew).id}
     assert_response :success
+    assert_nil flash[:error]
+  end
+  
+  test "should not get error on index" do
+    get :index
     assert_nil flash[:error]
   end
 end
