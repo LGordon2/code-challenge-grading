146d4c2fa2aaeb8662a6af57d376a561cccf836f
gso-admin <gso-admin@orasi.com>
2013-12-12 13:25:00 -0500

    Added things for precompiling on production.

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,20 @@ module CodingChallengeGrading
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    end
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ CodingChallengeGrading::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.precompile += %w[*.png]
 
   # Generate digests for assets URLs.
   config.assets.digest = true
