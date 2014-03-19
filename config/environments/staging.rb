193347d3d8e166c8b8cddc012d3bb0768a6645c9
lew <LGordon2@gmail.com>
2014-03-19 11:30:37 -0400

    Trying to get logs..

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -15,6 +15,8 @@ CodingChallengeGrading::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
+  
+  config.logger = Logger.new(STDOUT)
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
