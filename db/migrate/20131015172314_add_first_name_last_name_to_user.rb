761ff638c0efb292677a00ece4f5a5699d1afeea
lew <LGordon2@gmail.com>
2014-03-19 12:03:11 -0400

    integrated old migration code.

--- db/migrate/20131015172314_add_first_name_last_name_to_user.rb
+++ db/migrate/20131015172314_add_first_name_last_name_to_user.rb
@@ -1,6 +1,27 @@
 class AddFirstNameLastNameToUser < ActiveRecord::Migration
   def change
-    create_table :users
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+
+      t.timestamps
+    end
+    create_table :comments do |t|
+      t.string :name
+      t.string :comment
+      t.string :month
+      t.string :league
+
+      t.timestamps
+    end
+    create_table :submissions do |t|
+
+      t.timestamps
+    end
+    change_table :submissions do |t|
+      t.references :user
+    end
+    add_column :submissions, :submission_code, :text
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
   end
