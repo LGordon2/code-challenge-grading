b6ee361dee67003fad861f7f00e394822488f38e
mattwhooo <watson.mattc@orasi.com>
2014-03-21 09:47:38 -0400

    added complete checkbox

--- app/controllers/submission_controller.rb
+++ app/controllers/submission_controller.rb
@@ -1,4 +1,11 @@
 class SubmissionController < ApplicationController
   def destroy
   end
+  
+  def update
+    submission = Submission.find(params[:id])
+    submission.update(complete: params['submission']['complete'].to_s=="1")
+    submission.save
+  end
+  
 end
