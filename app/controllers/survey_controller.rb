875280442f34a1443e231230304281b4c1f5ad18
Mattwhooo <watson.mattc@gmail.com>
2014-04-12 22:40:28 -0400

    maze now works

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,3 +1,6 @@
+class Matrix
+  public :"[]=", :set_element, :set_component
+end
 class SurveyController < ApplicationController
 
     before_action :require_admin
@@ -26,207 +29,70 @@ class SurveyController < ApplicationController
 	end
 	
 	def maze
+		require 'matrix'
+		x_size = 25
+		y_size = 25
+		startPoint = [rand(y_size), rand(x_size)]
+		endPoint = [rand(y_size), rand(x_size)]
 		
-		options = ["N", "S", "E", "W", "NE", "NS", "NW", "ES", "EW", "WS", "NES", "NWS", "EWS", "NEW", "NEWS"]
-		maze = Hash.new(0)
-
-		use_random = true
-
-		maze_size = params[:size].to_i
-		maze_size.times do |i| 
-		  maze_size.times do |j|
-			if use_random
-			  maze[[i,j]] = options[rand(options.count)]
-			  use_random = false
-			else
-			  maze[[i,j]] = 1
-			  use_random = true
-			end
-		  end
-		end
-
-		maze_size.times do |y|
-		  maze_size.times do |x|
-			if maze[[x,y]] == 1
-			  up = maze[[x, y-1]]
-			  down = maze[[x,y+1]]
-			  left = maze[[x-1,y]]
-			  right = maze[[x+1,y]]
-			  current_cell = maze[[x,y]]
-			
-			  if up != 0
-				if up.include? "S"
-				  maze[[x,y]] = maze[[x,y]]== 1? "N":maze[[x,y]] + "N"    
-				end
-			  end
-			
-			  if right != 0
-				if right.include? "W"
-				  maze[[x,y]] = maze[[x,y]]== 1? "E":maze[[x,y]] + "E"    
-				end
-			  end
-			
-			  if down != 0
-				if down.include? "N"
-				  maze[[x,y]] = maze[[x,y]]== 1? "S":maze[[x,y]] + "S"    
-				end
-			  end
-			
-			  if left != 0
-				if left.include? "E"
-				  maze[[x,y]] = maze[[x,y]]== 1? "W":maze[[x,y]] + "W"    
-				end
-			  end
-			  
-			  if maze[[x,y]] == 1
-				maze[[x,y]] = "blank"
-			  end
-		   end    
-		  end
-		end
-		maze2 = maze
-		startPoint=[rand(maze_size), rand(maze_size)]
-		endPoint=[rand(maze_size), rand(maze_size)]
-		maze2[endPoint] = "point"
-		maze2[startPoint] = "point"
-		newstring = " "
-		 maze_size.times do |y|
-		  maze_size.times do |x|
-			newstring = newstring + maze2[[x,y]] + ","
-		  end
-		  newstring = newstring + ";"
-		end
+		direction_options = ["N", "S", "E", "W", "NE", "NS", "NW", "ES", "EW", "WS", "NES", "NWS", "EWS", "NEW", "NEWS"]
+		init_maze = Matrix.build(y_size,x_size) {|row, col| (row %2)==(col%2)? direction_options[rand(direction_options.count)]:0}
+		maze = Matrix.build(y_size,x_size){|row, col| (row %2)==(col%2)? init_maze[row,col]: complete_maze_from_neighbors(row,col, init_maze)}
+		display_maze = maze.clone
+		display_maze[startPoint[0], startPoint[1]] = "points"
+		display_maze[endPoint[0], endPoint[1]] = "point"
+		@start = maze[startPoint[0], startPoint[1]]
+		@end = maze[endPoint[0], endPoint[1]]
+		@maze_string = getString(display_maze)
 		
-		@newstring = newstring.gsub(" ", "")
+		counter = 1
 		
-		paths= Hash.new(0)
-		replace=""
-		current_replace = 0
-		path_counter = 1
-        
-		maze_size.times do |y|
-		  maze_size.times do |x|
-		   
-			up = maze[[x, y-1]]
-			down = maze[[x,y+1]]
-			left = maze[[x-1,y]]
-			right = maze[[x+1,y]]
-			
-			up_path = paths[[x, y-1]]
-			down_path = paths[[x,y+1]]
-			left_path = paths[[x-1,y]]
-			right_path = paths[[x+1,y]]
-			  
-			  if left == 0 and up == 0
-				
-				paths[[x,y]] = path_counter
-				path_counter = path_counter + 1
-			  
-			  elsif up == 0
-				if left.include? "E"
-				  paths[[x,y]] = left_path
+		paths = Matrix.build(y_size, x_size) {|row, col| 0}
+		paths.to_a.count.times do |row|
+			paths.to_a[0].count.times do |col|
+				if row == 0 and col == 0
+					#Top Left Corner
+					paths[row,col] = counter
+					counter += 1
+				elsif row == 0
+					#Top Row
+					if maze[row,col-1].include? "E"
+						paths[row,col] = paths[row,col-1]
+					else
+						paths[row,col] = counter
+						counter += 1
+					end
+				elsif col == 0
+					#Left Column
+					if maze[row-1,col].include? "S"
+						paths[row,col] = paths[row-1,col]
+					else
+						paths[row,col] = counter
+						counter += 1
+					end
 				else
-				  paths[[x,y]] = path_counter 
-				  path_counter = path_counter + 1
-				end 
-			  elsif left == 0
-				if up.include? "S"
-				  paths[[x,y]] = up_path
-				else
-				  paths[[x,y]] = path_counter
-				  path_counter = path_counter + 1
+					#Anywhere else
+					if maze[row,col-1].include? "E" and maze[row-1,col].include? "S"
+						#left									#up
+							paths[row,col] = paths[row-1,col] >= paths[row,col-1] ? paths[row,col-1] : paths[row-1,col] 
+												#up						#left			#left				#up
+							paths = replace_in_matrix(paths,paths[row,col-1],paths[row-1,col]) 
+																	#left		#up
+					elsif maze[row,col-1].include? "E"
+						paths[row,col] = paths[row,col-1]
+					elsif maze[row-1,col].include? "S"
+						paths[row,col] = paths[row-1,col]
+					else
+						paths[row,col] = counter
+						counter += 1
+					end
 				end
-			  else
-				if up.include? "S" and left.include?"E"
-				  if left_path.to_i > up_path.to_i
-					paths[[x,y]] = up_path
-					#replace = replace.to_s + left_path.to_s + "->"+ up_path.to_s + ","
-					replace_value(paths, maze_size, left_path, up_path)
-				  elsif up_path.to_i > left_path.to_i
-					paths[[x,y]] = left_path
-					#replace = replace.to_s + up_path.to_s + "->" + left_path.to_s + ","
-					replace_value(paths, maze_size, up_path, left_path)
-				  elsif up_path.to_i == left_path.to_i	
-					paths[[x,y]] = left_path
-				  end
-				elsif up.include? "S"
-					paths[[x,y]] = up_path
-		
-				elsif left.include? "E"
-				  paths[[x,y]] = left_path
-				else
-				  paths[[x,y]] = path_counter
-				  path_counter = path_counter + 1
-				end  
-			  end
-			end
-		end
-=begin
-		@oldReplace= replace
-		newReplace = Array.new
-		replace = replace.split(",")
-		missingLink = Hash.new
-		loop do 
-			# some code here
-			old = paths.to_a
-            
-			maze_size.times do |y|
-				maze_size.times do |x|
-					replace.reverse.each do |test|
-						newValue = test.split("->")
-						if newValue[0].to_i == paths[[x,y]].to_i
-							if missingLink[newValue[0]]
-								missingLink[newValue[0]].split(";").each do |link|
-									if link.to_i > newValue[1].to_i
-										newReplace.unshift(link.to_s + "->" + newValue[1].to_s) unless newReplace.include? link.to_s + "->" + newValue[1].to_s
-									else
-										newReplace.unshift(newValue[1].to_s + "->" + link.to_s) unless newReplace.include? newValue[1].to_s + "->" + link.to_s
-									end
-								end
-								missingLink[newValue[0]] = missingLink[newValue[0]].to_s + ";" + newValue[1].to_s unless missingLink[newValue[0]].include? newValue[1].to_s
-							else
-								missingLink[newValue[0]] = newValue[1].to_s
-							end
-							missingLink[newValue[0]] =missingLink[newValue[0]].to_s + ";" + newValue[1].to_s 
-							paths[[x,y]] = newValue[1]
-						end
-					end	  
-				end
-			end
-			newReplace.each do |new|
-				replace.unshift(new) unless replace.include? new
-			end
-			break if old.eql? paths.to_a
-		end
-=end
-		pathString = " "
-		 maze_size.times do |y|
-		  maze_size.times do |x|
-			pathString = pathString + paths[[x,y]].to_s + ","
-		  end
-		  pathString = pathString + ";"
-		end
-		
-		@pathString = pathString.gsub(" ", "")
-		if paths[startPoint].to_i == paths[endPoint].to_i
-			@result = "A Path Does Exist"
+			end	
 			
-		else
-			@result = "A Path Does NOT Exist"
 		end
-    	@value1 = paths[startPoint].to_i
-			@value2 = paths[endPoint].to_i
-	end
-	
-	private
-	def replace_value(hash, size, old, new)
-		size.times do |y|
-			size.times do |x|
-				if hash[[x,y]].to_i == old.to_i
-					hash[[x,y]] = new.to_i
-				end  
-			end
-		end
+		
+		@path_string = getString(paths)
+		@result = paths[startPoint[0], startPoint[1]] == paths[endPoint[0], endPoint[1]] 
 	end
 
 	def require_admin
@@ -236,4 +102,50 @@ class SurveyController < ApplicationController
 	def survey_params
 		params.require(:survey).permit(:haveParticipated,:whyStart, :newSkills, :howUsedSkills, :skillsExample, :whyNot, :whatWouldInterest, :whatWouldIncreaseParticipation, :bestLeague, :automation, :otherDevelopment, :betterServe)
 	end
+	
+	#Private
+	
+	def replace_in_matrix( matrix, val1, val2)
+		if val1 > val2
+			matrix.collect{ |m| m == val1 ? val2 : m}
+		else
+			matrix.collect{ |m| m == val2 ? val1 : m}
+		end
+	end
+	
+	def getString(matrix)
+		output_string = ""
+		matrix.row_vectors.each do |r|
+		  output_string = output_string + r.to_a.join(",") + ";"
+		end
+		output_string
+	end
+	
+	def complete_maze_from_neighbors(row,col, maze)
+		
+		result = ""
+		
+		if row != 0 and maze[row-1,col] and maze[row-1,col].include? "S"
+			result = result + "N"
+		end
+		
+		if col != (maze.to_a[0].count) and maze[row,col+1] and maze[row,col+1].include? "W"
+			result = result + "E"
+		end
+		
+		if row != (maze.to_a.count) and maze[row+1,col] and maze[row+1,col].include? "N"
+			result = result + "S"
+		end
+		
+		if col != 0 and maze[row,col-1] and maze[row,col-1].include? "E"
+			result = result + "W"
+		end
+		
+		if result == ""
+			result = "blank"
+		end
+		
+		result
+	end
+		
 end
