7e0567f6bb79793c4f81232e764f8c491a0e8ec9
lew <LGordon2@gmail.com>
2014-05-21 14:53:59 -0400

    Fix for path hacking.

--- app/controllers/submission_controller.rb
+++ app/controllers/submission_controller.rb
@@ -1,21 +1,34 @@
 class SubmissionController < ApplicationController
+  before_action :set_submission, only: [:show, :update]
+  before_action :require_user_or_admin, only: [:show, :update]
+  
   def destroy
   end
+  
   def show
-	@submission = Submission.find(params[:id])
   end
+  
   def update
-    submission = Submission.find(params[:id])
-    submission.update(complete: params['submission']['complete'].to_s=="1")
-    submission.save
+    @submission.update(complete: params['submission']['complete'].to_s=="1")
+    @submission.save
   end
   def winner
-	submission = Submission.find(params[:winner][:submission_id])
-	#winner = submission.challenge.challenge_winners.find_by(challenge_id: submission.challenge.id,submission_id: submission.id)
-	if params[:winner][:isWinner].to_s == "1"
-		submission.challenge.challenge_winners.create(challenge_id: submission.challenge.id,submission_id: submission.id)
-	else
-		submission.challenge.challenge_winners.find_by(challenge_id: submission.challenge.id,submission_id: submission.id).destroy
-	end
+  	submission = Submission.find(params[:winner][:submission_id])
+  	#winner = submission.challenge.challenge_winners.find_by(challenge_id: submission.challenge.id,submission_id: submission.id)
+  	if params[:winner][:isWinner].to_s == "1"
+  		submission.challenge.challenge_winners.create(challenge_id: submission.challenge.id,submission_id: submission.id)
+  	else
+  		submission.challenge.challenge_winners.find_by(challenge_id: submission.challenge.id,submission_id: submission.id).destroy
+  	end
+  end
+  
+  private
+  
+  def set_submission
+    @submission = Submission.find(params[:id])
+  end
+  
+  def require_user_or_admin
+    redirect_to :root unless current_user.admin? or current_user == @submission.user
   end
 end
